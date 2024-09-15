733e31774001100e539a6de6513314d9f2d8a7dc
Soukful <jyotsanaverma2005@gmail.com>
2024-09-15 15:28:49 +0530

    Text To Speech Model 1

---
+++ TextToSpeech/TextToSpeech.py
@@ -0,0 +1,13 @@
+import pyttsx3
+def speak(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
+
+text = "Hello I am Jarvis AI"
+speak(text)
+
